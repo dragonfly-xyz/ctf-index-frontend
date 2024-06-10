1129f211cd49eaf5a3c9e668927be69134ecf6a3
Lawrence Forman <me@merklejerk.com>
2024-06-10 02:08:45 -0400

    create site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,111 @@
+<style lang="scss">
+    @use '../lib/styles/global.scss' as *;
+    @use '../lib/styles/breakpoints.scss' as *;
+
+    :global(body) {
+        background-color: black;
+        padding: 0;
+        margin: 0;
+        border: min(2vw, 2vh) solid var(--color-secondary);
+        box-sizing: border-box;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+
+        @include mobile {
+            border-left: none;
+            border-right: none;
+            border-bottom-width: 2vh;
+        }
+    }
+
+    main {
+        flex: 1 1 auto;
+        font-family: var(--font-primary);
+        color: var(--color-primary);
+        font-size: 1.33em;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h1 {
+        margin: 0;
+        padding: 0 2ex 0.5em 4ex;
+        background-color: var(--color-secondary);
+        font-family: var(--font-title);
+        font-weight: bold;;
+        text-transform: uppercase;
+        letter-spacing: -0.2ex;
+        display: flex;
+        justify-content: space-between;
+        font-size: 2.25em;
+
+        @include mobile {
+            font-size: 1.75em;
+        }
+    }
+
+    .logo {
+        letter-spacing: 0.05ex;
+        font-weight: bolder;
+    }
+
+    .smaller {
+        font-size: 0.8em;
+    }
+
+    .title {
+        letter-spacing: 0.05ex;
+        color: white;
+
+        &.mobile {
+            display: none;
+        }
+
+        @include mobile {
+            display: none;
+            &.mobile {
+                display: initial;
+            }
+        }
+    }
+
+    .home {
+        text-decoration: none;
+        color: inherit;
+        font-weight: inherit;
+        display: flex;
+        gap: 2ex;
+        transition: transform 0.1s ease-out;
+
+        &:hover {
+            color: white;
+            transform: translate(1ex, 0);
+        }
+
+        > .logo {
+            margin-top: -0.1em;
+        }
+    }
+</style>
+
+<h1>
+    <span class="title mobile">
+        ./CTFs
+    </span>
+    <span class="title desktop">
+        ./CAPTURE_THE_FLAGS
+    </span>
+    <a class="home smaller" href="https://dragonfly.xyz">
+        <span>Dragonfly</span>
+        <span class="logo">
+            &gt;|&lt;
+        </span>
+    </a>
+</h1>
+<main>
+    <slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,108 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import MdLaunch from 'svelte-icons/md/MdLaunch.svelte';
+
+    function navTo(uri: string): void {
+        window.location.href = uri;
+    }
+</script>
+
+<style lang="scss">
+    @use '../lib/styles/global.scss' as *;
+    @use '../lib/styles/breakpoints.scss' as *;
+    @use 'sass:map';
+
+    :root {
+        --highlight-color: var(--color-secondary);
+    }
+
+    .content {
+        flex: 1 1 auto;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        flex-direction: column;
+        
+        > .contest {
+            width: 100%;
+            flex: 1 1 auto;
+            margin: 0;
+            font-family: inherit;
+            font-weight: inherit;
+            font-size: inherit;
+            text-align: inherit;
+            border: none;
+            vertical-align: inherit;
+            cursor: pointer;
+            background: none;
+            color: inherit;
+            padding: 8ex;
+            
+            &:hover {
+                background-color: var(--highlight-color);
+                color: black;
+            }
+
+            &:hover > .body {
+                transform: scale(1.05);
+            }
+            
+            > .body {
+                margin: 0 auto;
+                max-width: map.get(map.get($breakpoints, 'md'), 'breakpoint');
+                transition: transform 0.1s ease-out;
+
+                > h2 {
+                    margin: 0;
+                    font-style: var(--font-pixel);
+                    text-decoration: underline;
+                }
+    
+                > .date {
+                    margin-left: 2ex;
+                    font-style: italic;
+                }
+    
+                > .link-icon {
+                    float:right;
+                    height: 1.5em;
+                }
+    
+                > .description {
+                    text-align: justify;
+                }
+            }
+
+            &:not(:last-child) {
+                border-bottom: 0.125em dashed var(--highlight-color);
+            }
+        }
+    }
+</style>
+
+<div class="content">
+    <button class="contest" on:click={() => navTo('https://dragonfly-xyz.github.io/nottingham-frontend')}>
+        <div class="body">
+            <div class="link-icon">
+                <MdLaunch />
+            </div>
+            <h2>Searchers of Nottingham</h2>
+            <div class="date">06/15/2024</div>
+            <div class="description">
+                Submit smart contract agents to compete against other merchants at a medieval faire in this multiplayer, MEV-themed game.
+            </div>
+        </div>
+    </button>
+    <button class="contest" on:click={() => navTo('https://ctf.dragonfly.xyz')}>
+        <div class="body">
+            <div class="link-icon">
+                <MdLaunch />
+            </div>
+            <h2>Puzzlebox</h2>
+            <div class="date">05/05/2023</div>
+            <div class="description">
+                Can you unlock this puzzlebox of fiendishly clever, inteconnected Solidity puzzles? How efficently can you do it?
+            </div>
+        </div>
+    </button>
+</div>
